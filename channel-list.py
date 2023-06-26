4176813e26be813319592b0c2e4a3d8fd48b035d
ssfuno <96967819+ssfuno@users.noreply.github.com>
2023-06-26 21:15:17 +0900

    add title exporting feature and fix some issues

--- channel-list.py
+++ channel-list.py
@@ -1,8 +1,10 @@
-from tokenize import generate_tokens
-import requests
+import csv
 import pdb
 import re
-import csv
+from tokenize import generate_tokens
+
+from bs4 import BeautifulSoup
+import requests
 
 SLACK_CHANNEL_ID = 'C057JGRUJDB'
 SLACK_URL = "https://slack.com/api/conversations.history"
@@ -10,47 +12,56 @@ SLACK_URL = "https://slack.com/api/conversations.history"
 TOKEN = "hoge"
 
 def get_https_strings(text):
-    pattern = "https?://[\w/:%#\$&\?\(\)~\.=\+\-]+"
+    pattern = "(https?://.+?)>"
     https_strings = re.findall(pattern, text)
+    https_strings = list(map(lambda x: re.sub('\|.+', '', x), https_strings))
     return https_strings
 
-def get_urls():
+def get_title(url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    # Twitterの投稿などでtitle要素が存在しないときは'タイトル'を返す
+    try:
+        return soup.title.text
+    except:
+        return 'タイトル'
+
+def get_contents():
     payload = {
         "channel": SLACK_CHANNEL_ID,
         "oldest": "1622761200"
     }
     headersAuth = {
     'Authorization': 'Bearer '+ str(TOKEN),
-    }  
+    }
     response = requests.get(SLACK_URL, headers=headersAuth, params=payload)
     json_data = response.json()
     msgs = json_data['messages']
-    urls = []
+    contents = []
     index = 1
     for msg in msgs:
-      # slackチャンネルのURLは含めない
-      if not ("https://saito-mentee" in msg["text"]) and (len(get_https_strings(msg["text"])) > 0):
-        # 同じスレッドに複数のURLがあることを考慮
-        for text in get_https_strings(msg["text"]):
-          urls.append([index, text])
+      # slackチャンネルとNotionページのURLは含めない
+      if not ("https://saito-mentee" in msg["text"] or "https://www.notion.so" in msg["text"]) and (len(get_https_strings(msg["text"])) > 0):
+        for url in get_https_strings(msg["text"]):
+          title = get_title(url)
+          contents.append([title, index, url])
           index = index + 1
-
-    print(urls)
-    return urls
+    print(contents)
+    return contents
 
 def generate_csv():
-  postred_urls = get_urls()
+  posted_contents = get_contents()
 
   # CSVファイルのパス
-  csv_file_path = 'posted_urls.csv'
+  csv_file_path = 'posted_contents.csv'
 
   # CSVファイルを書き込みモードで開く
   with open(csv_file_path, 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
-    writer.writerow(["番号", "URL"])
+    writer.writerow(["名前", "番号", "URL"])
 
     # 配列の各行をCSVファイルに書き込む
-    for row in postred_urls:
+    for row in posted_contents:
         writer.writerow(row)
 
   print(f'CSVファイル "{csv_file_path}" が生成されました。')
